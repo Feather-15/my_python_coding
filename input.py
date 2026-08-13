829c889dcdf572029c1c8f9ed6ab421a02f8219a
Saipraveen <saipraveen.jabade@gmail.com>
2026-08-13 19:28:28 +0530

    swap with temp

---
+++ input.py
@@ -0,0 +1,4 @@
+print("enter your name")
+name= input()
+age= input()
+print ("welcome", name , age, "to Python coding")
---
+++ swapping.py
@@ -0,0 +1,7 @@
+var1 = input("enter the value 1")
+var2 = input("enter the value 2")
+temp=0
+temp=var1
+var1=var2
+var2=temp
+print (var1, var2)
