dff18e01060a8972015eb48a21ab8987acae1445
Saipraveen <saipraveen.jabade@gmail.com>
2026-08-15 01:45:21 +0530

    *

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+inp= int(input("enter the number"))
+firstnum = 0
+secondnum = 1
+sum
+print("0")
+print("1")
+for inp in range (0, inp+1) : 
+    sum = firstnum+secondnum
+    firstnum = secondnum
+    secondnum = sum
+    print(sum)
+
