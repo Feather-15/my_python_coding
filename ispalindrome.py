ea5eacc7a21d8d2c49a7b8eab5af327b8baf8603
Saipraveen <saipraveen.jabade@gmail.com>
2026-08-15 18:07:58 +0530

    update

--- ispalindrome.py
+++ ispalindrome.py
@@ -1,11 +1,19 @@
 intext = int(input("enter the number"))
 if intext <0 :
-    print (intext," is not a palindrome number")   
+    print (intext," is not a palindrome number") 
+temp = intext  
 rev = 0
 
 while intext != 0 : #intext = 1512
     reverse= intext%10
     rev= (rev)*10 + (reverse)
-    intext = intext //10
-print (rev)
+    intext = intext //10 #here it is floor division
+    
+    
+if temp==rev:
+    print("Ispalindrome")
+else:
+    print("Not a Palindrome")
+    
+ #print (rev)
     
